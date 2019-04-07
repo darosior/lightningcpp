7cddab13d94102fdd7fddec83ee7a87680a4e469
darosior <darosior@protonmail.com>
2019-04-07 20:00:17 +0200

    Corrected some typos

--- src/clightningwrapper.h
+++ src/clightningwrapper.h
@@ -14,12 +14,12 @@ private:
     
 public:
     CLightningWrapper(std::string& socket_path);
-    ~CLightningWrapper;
+    ~CLightningWrapper();
 
     /** Sends a JSON-RPC command to the C-Lightning socket.
      * Used by all following functions to communicate with lightningd.
      */
-    Json::Value sendCommand(const std::string& comand, const Json::Value& arguments);
+    Json::Value sendCommand(const std::string& command, const Json::Value& arguments);
 
     /* Boolean functions are for calls that don't return anything usefull but for which it can be usefull to
      * return a confirmation (true) after execution (some of these commands can take time, like `pay`). */
@@ -35,7 +35,7 @@ public:
     bool fundChannel(const std::string& id, const unsigned int& sats, const unsigned int& feerate=0,
             const unsigned int& minconf=0);
     bool pay(const std::string& bolt11, const unsigned int& msat=0, const std::string& label="",
-            const unsigned int& riskfactor=10, const unsigned float& maxFeePercent=0.5,
+            const unsigned int& riskfactor=10, const float& maxFeePercent=0.5,
             const unsigned int& retryFor=60, const unsigned int& maxDelay=500, const unsigned int exemptFee=5000);
     bool ping(const std::string& id, const unsigned int& len=128, const unsigned int& pongbytes=128);
     bool stop();
@@ -50,7 +50,7 @@ public:
 
     std::string getConfig(const std::string& param);
     std::string getInvoice(const unsigned int& msat, const std::string& label, const std::string& description,
-            const unsigned int& expiry=3600, const std::vector<std::string>& fallbacks,
+            const unsigned int& expiry=3600, const std::vector<std::string>& fallbacks={},
             const std::string& preimage="");
     std::string help();
     std::string helpOn(const std::string& command); // man page
@@ -61,14 +61,14 @@ public:
     /* JSON Value functions are for calls whether complex, long, that need the JSON syntax (used in lightningd), or simply
      * not yet implemented.*/
 
-    Json::Value decodepay(const sts::string& bolt11);
+    Json::Value decodepay(const std::string& bolt11);
     Json::Value getFeerate(const std::string& style);
     Json::Value getInfo();
     Json::Value getLog(const std::string& level="info");
     Json::Value getRoute(const std::string& id, const unsigned int& msatoshi, const unsigned int& riskfactor, 
-            const unsigned int& cltv=9, const std::string& fromid="", const unsigned float& fuzzPercent=5,
+            const unsigned int& cltv=9, const std::string& fromid="", const float& fuzzPercent=5,
             const std::vector<std::string>& exlude={}, const unsigned int& maxhops=20);
-    Json::value listChannels(const std::string& shortChannelId="", const std::string& source="");
+    Json::Value listChannels(const std::string& shortChannelId="", const std::string& source="");
     Json::Value listConfigs();
     Json::Value listForwards();
     Json::Value listFunds();
